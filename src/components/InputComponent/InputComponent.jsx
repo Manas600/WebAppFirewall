f182a764e9cb8aedcc1ed5623f5aa316d035898c
Manas Hardikar <hardikarmanas600@gmail.com>
2024-08-31 10:24:03 +0530

    Second

--- src/components/InputComponent/InputComponent.jsx
+++ src/components/InputComponent/InputComponent.jsx
@@ -1,15 +1,19 @@
 import s from "./style.module.css";
 
 export function InputComponent({ inputText, setInputText }) {
+  const handleInput = (e) => {
+    setInputText(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleButton = () => {
+    console.log("Button clicked with input", inputText);
+  };
+
   return (
     <div className={s.container}>
       <p>Test for malicious payload. Insert Payload below</p>
-      <input
-        type="text"
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
-      <button>Check Payload</button>
+      <input type="text" value={inputText} onChange={handleInput} />
+      <button onClick={handleButton}>Check Payload</button>
     </div>
   );
 }
